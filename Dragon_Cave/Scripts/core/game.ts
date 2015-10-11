3088c034ba4cc3c9e5d7cecae1ff9f2d0212fd0a
liliya0artyuh <liliya0artyukh@gmail.com>
2015-10-10 20:55:11 -0400

    initial commit

---
+++ Dragon_Cave/Scripts/core/game.ts
@@ -0,0 +1,110 @@
+﻿/// <reference path="../typings/jquery/jquery.d.ts" />
+/// <reference path="../typings/stats/stats.d.ts" />
+/// <reference path="../typings/createjs-lib/createjs-lib.d.ts" />
+/// <reference path="../typings/easeljs/easeljs.d.ts" />
+/// <reference path="../typings/tweenjs/tweenjs.d.ts" />
+/// <reference path="../typings/webaudioapi/waa.d.ts" />
+
+/// <reference path="../config/config.ts" />
+
+/// <reference path="../objects/label.ts" />
+/// <reference path="../objects/button.ts" />
+/// <reference path="../states/menu.ts" />
+
+
+// Global Game Framework Variables
+var canvas: HTMLElement;
+var stage: createjs.Stage;
+var stats: Stats;
+var state: number;
+var scene: createjs.Container;
+var stateFunction: any; // alias for our current state
+
+//var assets: createjs.LoadQueue;
+
+var manifest = [
+    { id: "startButton", scr: "Assets/images/startButton.png" },
+    { id: "logo", scr: "Assets/images/logo_small.png" },
+    { id: "gameSound", scr: "Assets/audio/gameSound.m4a" }
+];
+
+// Game variables
+var helloLabel: objects.Label;
+var introLabel: objects.Label;
+var startButton: objects.Button;
+var logo: createjs.Bitmap;
+
+
+function preload(): void {
+ //   assets = new createjs.LoadQueue();
+    //assets.installPlugin(createjs.Sound);
+    //event listener triggers when assets are completely loaded
+   // assets.on("complete", init, this);
+   // assets.loadManifest(manifest);
+
+
+}
+
+function init(): void {
+    console.log("Game started...");
+    canvas = document.getElementById("canvas"); // reference to canvas element
+    stage = new createjs.Stage(canvas); // passing canvas to stage 
+    stage.enableMouseOver(20); //enable mouse events
+    createjs.Ticker.setFPS(60); // set frame rate to 60 frames per second
+    createjs.Ticker.on("tick", gameLoop, this); // update gameLoop every frame
+        
+    setupStats();// setup statistics object
+
+    state = config.MENU_STATE;
+    changeState();
+}
+
+// Main Game Loop
+function gameLoop(event: createjs.Event): void {
+    stats.begin(); //begin measuring
+
+    stage.update(); // redraw/refresh stage every frame
+
+    stats.end(); // end measuring
+}
+
+// function to setup stat counting
+function setupStats(): void {
+    stats = new Stats();
+    stats.setMode(0); //show fps
+
+    //align bottom-right
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.right = '0px';
+    stats.domElement.style.top = '0px';
+
+  //  document.getElementById("main").appendChild(stats.domElement);
+    document.body.appendChild(stats.domElement);
+}
+
+//callback function that allows to respond to button click events
+function buttonClicked(event: createjs.MouseEvent): void {
+    introLabel.text = "clicked!";
+  //  createjs.Sound.play("gameSound");
+}
+
+
+// state machine
+function changeState(): void {
+    //lauch various scenes
+
+    switch (state) {
+        case config.MENU_STATE:
+            stateFunction = states.menu;
+            break;
+        case config.PLAY_STATE:
+
+            break;
+
+        case config.OVER_STATE:
+
+            break;
+    }
+    stateFunction();
+
+}
---
+++ Dragon_Cave/Scripts/states/menu.ts
@@ -0,0 +1,25 @@
+﻿// menu state
+module states {
+    // this is where all the fun happens
+    export function menu(): void {
+        scene = new createjs.Container();
+        console.log("Game is running");
+        //instantiate and add a logo
+        logo = new createjs.Bitmap("../../Assets/images/logo_small.png");
+        logo.regX = 155;
+        logo.regY = 101;
+        logo.x = 420;
+        logo.y = 140;
+        scene.addChild(logo);
+        //helloLabel = new objects.Label("Hello Friend", "60px Consolas", "#000000", 320, 120);
+        //stage.addChild(helloLabel); // add label to the stage
+        introLabel = new objects.Label(" Do you want to play?", "40px Consolas", "#000000", 320, 260);
+        scene.addChild(introLabel);
+        //instantiate and add a start button
+        startButton = new objects.Button("startButton", 320, 340);
+        scene.addChild(startButton);
+        startButton.on("click", buttonClicked, this);
+        stage.addChild(scene);
+    }
+
+}
